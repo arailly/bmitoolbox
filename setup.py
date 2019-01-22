21e00a410b0f5842e46e2d4f7cc2f28f8dff74af
Yusuke ARAI <txtbokwrm@gmail.com>
2019-01-22 12:29:46 +0900

    :tada: first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='bmitoolbox',
+    version='0.0.0',
+    packages=['bmitoolbox'],
+    url='',
+    license='',
+    author='Yusuke ARAI',
+    author_email='',
+    description=''
+)
