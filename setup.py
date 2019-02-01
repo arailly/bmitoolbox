cb52b97955f63acfa32e2c4c08a6311f996f8710
Yusuke ARAI <txtbokwrm@gmail.com>
2019-02-01 17:23:47 +0900

    :bug: modified __init__.py
    i don't understand usage `packages` ...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='bmitoolbox',
     version='0.0.0',
-    packages=['bmitoolbox'],
+    # packages=['bmitoolbox'],
     url='',
     license='',
     author='Yusuke ARAI',
