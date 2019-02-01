f154092ed5a6ddbe128532d5862b8f05ccdcb6c8
Yusuke ARAI <txtbokwrm@gmail.com>
2019-02-01 17:41:52 +0900

    :bug: modified __init__.py and experiment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='bmitoolbox',
     version='0.0.0',
-    packages=['bmitoolbox', 'bmitoolbox.preprocess'],
+    packages=['bmitoolbox.preprocess'],
     url='',
     license='',
     author='Yusuke ARAI',
