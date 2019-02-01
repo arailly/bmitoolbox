f13e3a7e43cbe287b217e56455cca2512b892f48
Yusuke ARAI <txtbokwrm@gmail.com>
2019-02-01 17:53:09 +0900

    :bug: modified __init__.py and experiment

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
+import os
+
+def subpackages(root_dir):
+    packages = [
+        root_dir + '.' + dir_name
+        for dir_name in os.listdir(root_dir)
+        if dir_name != '__init__.py'
+    ]
+    return packages
 
 setup(
     name='bmitoolbox',
     version='0.0.0',
-    packages=['bmitoolbox', 'bmitoolbox.preprocess'],
+    packages=['bmitoolbox'] + subpackages('bmitoolbox'),
     url='',
     license='',
     author='Yusuke ARAI',
