19530ca23af10bbe8d60d51173bd90e3919f2a95
Yusuke ARAI <txtbokwrm@gmail.com>
2019-02-13 16:34:05 +0900

    :+1: added test code

---
+++ test/src/preprocess/signal_test.py
@@ -0,0 +1,16 @@
+import unittest
+import bmitoolbox as bt
+import numpy as np
+
+class Signal(unittest.TestCase):
+
+    def test_epoching(self):
+        array = np.array(list(range(100))) * 10
+        triggers = [0, 10, 20]
+        actual = bt.epoching(raw=array, trig=triggers, sfreq=2)
+        expect = np.array([[0, 10], [100, 110], [200, 210]])
+        self.assertEqual(expect.size, np.sum(actual == expect))
+
+
+if __name__ == '__main__':
+    unittest.main()
